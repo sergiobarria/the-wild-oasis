fc1815adddbc2cc02e6d2266ca5451332d124feb
sergiobarria <sbarria.dev@gmail.com>
2023-11-22 22:57:09 -0500

    add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import { Button } from './components/ui';
+
 export default function App() {
-    return <h1 className="text-3xl font-semibold underline">Hello world!</h1>;
+    return (
+        <h1 className="text-3xl font-semibold underline">
+            <Button onClick={() => alert('Button working')}>Click me</Button>
+        </h1>
+    );
 }
