01eb2aedf190dce45d78ad1cab57da8d1febb0cb
sergiobarria <sbarria.dev@gmail.com>
2023-12-03 23:10:35 -0500

    Initial setup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+</script>
+
+<h1 class="text-3xl font-bold text-red-500 underline">Hello world!</h1>
+<Button variant="default" on:click={() => alert('CLICKED!')}>Click me</Button>
