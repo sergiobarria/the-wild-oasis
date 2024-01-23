5702e29a7da6ca96f1f2a641e4817d017b423b7f
sergiobarria <sbarria.dev@gmail.com>
2024-01-22 22:00:29 -0500

    initial project setup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
+</script>
+
+<h1 class="text-red-500">Welcome to SvelteKit</h1>
+<Button variant="outline" on:click={() => console.log('CLICKED!')}>Click Me</Button>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
