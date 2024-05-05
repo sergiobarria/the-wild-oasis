f5e7c8b592c3457851c464653656ccee20679244
sergiobarria <sbarria.dev@gmail.com>
2024-05-05 09:47:39 -0500

    remove tooltip from theme toggle btn

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -6,7 +6,7 @@ import {
 	DropdownMenuItem,
 	DropdownMenuTrigger
 } from '@/components/ui/dropdown-menu';
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
+
 import { useTheme } from '@/components/theme-provider';
 
 export function ThemeToggle() {
@@ -15,19 +15,13 @@ export function ThemeToggle() {
 	return (
 		<DropdownMenu>
 			<DropdownMenuTrigger asChild>
-				<TooltipProvider>
-					<Tooltip>
-						<TooltipTrigger asChild>
-							<button className="flex items-center rounded-lg p-2 text-muted-foreground hover:bg-gray-200 dark:hover:text-black">
-								<SunIcon className="size-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-								<MoonStarIcon className="absolute size-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-							</button>
-						</TooltipTrigger>
-						<TooltipContent side="right">
-							<p className="p-0 text-xs capitalize">Toggle Theme</p>
-						</TooltipContent>
-					</Tooltip>
-				</TooltipProvider>
+				<button
+					className="flex items-center rounded-lg p-2 text-muted-foreground hover:bg-gray-200 dark:hover:text-black"
+					title="Toggle Theme"
+				>
+					<SunIcon className="size-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+					<MoonStarIcon className="absolute size-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+				</button>
 			</DropdownMenuTrigger>
 
 			<DropdownMenuContent align="end">
