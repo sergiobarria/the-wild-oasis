acf20ef15a6d77939d109063ba4c1074c7e9fc30
sergiobarria <sbarria.dev@gmail.com>
2024-05-07 01:36:58 -0500

    add ui components

--- src/lib/components/ui/dropdown-menu/dropdown-menu-shortcut.svelte
+++ src/lib/components/ui/dropdown-menu/dropdown-menu-shortcut.svelte
@@ -2,12 +2,12 @@
 	import type { HTMLAttributes } from 'svelte/elements';
 	import { cn } from '$lib/utils.js';
 
-	type $$Props = HTMLAttributes<HTMLTableSectionElement>;
+	type $$Props = HTMLAttributes<HTMLSpanElement>;
 
 	let className: $$Props['class'] = undefined;
 	export { className as class };
 </script>
 
-<tbody class={cn('[&_tr:last-child]:border-0', className)} {...$$restProps}>
+<span class={cn('ml-auto text-xs tracking-widest opacity-60', className)} {...$$restProps}>
 	<slot />
-</tbody>
+</span>
