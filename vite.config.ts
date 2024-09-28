96c120a6574cae86bf9d5dbe9add4694c9672e36
sergiobarria <sbarria.dev@gmail.com>
2024-09-27 19:14:12 -0500

    setup new project

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,6 @@
-import path from 'path'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
-			'~': path.resolve(__dirname, './convex'),
-		},
-	},
-})
+	plugins: [sveltekit()]
+});
