82af0e0a2fb25bc47325043c010e177a99c27221
sergiobarria <sbarria.dev@gmail.com>
2024-11-23 23:07:28 -0500

    new sveltekit project setup

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,21 +1,16 @@
-import * as dotenv from 'dotenv';
 import { defineConfig } from 'drizzle-kit';
-
-dotenv.config({ path: '.env.local' });
-
-const { DATABASE_URL, DATABASE_TOKEN } = process.env;
-
-if (!DATABASE_URL) {
-	throw new Error('Database credentials must be provided');
-}
+if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
 export default defineConfig({
-	schema: 'src/database/schemas',
-	out: 'src/database/migrations',
-	dialect: 'sqlite',
-	driver: 'turso',
+	schema: './src/lib/server/db/schema.ts',
+
 	dbCredentials: {
-		url: DATABASE_URL,
-		authToken: DATABASE_TOKEN,
+		url: process.env.DATABASE_URL,
+		authToken: process.env.DATABASE_AUTH_TOKEN
 	},
+
+	verbose: true,
+	strict: true,
+	driver: 'turso',
+	dialect: 'sqlite'
 });
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<title>The Wild Oasis</title>
+</svelte:head>
+
+{@render children()}
