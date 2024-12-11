bd5c44f69bf4097813ee90591c3afbe48ae2bf37
sergiobarria <sbarria.dev@gmail.com>
2024-12-11 18:42:08 -0500

    remove old project

--- src/routes/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-	import { Button } from '$lib/components/ui/button';
-</script>
-
-<Button href="/dashboard">Go to Dashboard</Button>
--- src/routes/dashboard/cabins/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import type { Cabin } from '$lib/features/cabins/types';
-
-import type { PageServerLoad } from './$types';
-
-export const load: PageServerLoad = async ({ locals }) => {
-	const cabins = await locals.pb.collection<Cabin>('cabins').getFullList();
-	console.log('🚀 ~ constload:PageServerLoad= ~ cabins:', cabins);
-	return { cabins };
-};
