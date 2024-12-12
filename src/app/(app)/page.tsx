e8d4caa789b1c5726bc74db25f44efcaaa5b07a9
sergiobarria <sbarria.dev@gmail.com>
2024-12-11 19:04:04 -0500

    initial project setup

---
+++ src/app/(app)/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+
+export default function HomePage() {
+	return (
+		<div>
+			helloowwww!
+			<Button onClick={() => alert('IT WORKS!!!')}>Click me</Button>
+		</div>
+	);
+}
