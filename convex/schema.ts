be377ea8101a83c095f21499948c0a9e8319d549
sergiobarria <sbarria.dev@gmail.com>
2025-03-01 12:52:22 -0500

    delete old

--- convex/schema.ts
+++
@@ -1,9 +0,0 @@
-import { defineSchema, defineTable } from 'convex/server';
-import { v } from 'convex/values';
-
-export default defineSchema({
-	cabins: defineTable({
-		name: v.string(),
-		price: v.float64(),
-	}),
-});
