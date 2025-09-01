c0307f3660e56ba0d8175d2af9d5a43f965cd84d
sbcar01 <sergio.barria@oceanscode.com>
2025-09-01 15:43:33 -0500

    delete: old laravel project

--- config/booking.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'fees' => [
-        'booking' => env('BOOKING_FEE', 25),
-        'processing' => env('PROCESSING_FEE', 2.9),
-        'tax' => env('SALES_TAX', 7),
-    ],
-];
--- routes/api.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::post('stripe/webhook', [\App\Http\Controllers\StripeWebhookController::class, 'handle']);
