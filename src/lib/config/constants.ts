dca11d0c37e6570d6d52ef460134eae555b164da
sergiobarria <sbarria.dev@gmail.com>
2025-10-10 17:56:47 -0500

    delete old version

--- src/lib/config/constants.ts
+++
@@ -1,4 +0,0 @@
-export const APP_NAME = 'The Wild Oasis';
-export const APP_DESCRIPTION =
-	'The Wild Oasis is a cozy cabin rental in the heart of the wilderness.';
-export const APP_URL = 'https://the-wild-oasis.com';
--- src/routes/(web)/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-	import ThemeToggle from '$lib/components/shared/theme-toggle.svelte';
-	import { Button } from '$lib/components/ui/button';
-</script>
-
-<ThemeToggle />
-<Button href="/admin">Dashboard</Button>
