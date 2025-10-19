b017a87e446a933036bbc9b9322008511d8e3875
sergiobarria <sbarria.dev@gmail.com>
2025-10-19 18:56:54 -0500

    fix text area component so the rows prop works properly

--- src/lib/components/ui/textarea/textarea.svelte
+++ src/lib/components/ui/textarea/textarea.svelte
@@ -7,6 +7,7 @@
 		ref = $bindable(null),
 		value = $bindable(),
 		class: className,
+		rows,
 		'data-slot': dataSlot = 'textarea',
 		...restProps
 	}: WithoutChildren<WithElementRef<HTMLTextareaAttributes>> = $props();
@@ -16,9 +17,11 @@
 	bind:this={ref}
 	data-slot={dataSlot}
 	class={cn(
-		'flex field-sizing-content min-h-16 w-full rounded-md border border-input bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 md:text-sm dark:bg-input/30 dark:aria-invalid:ring-destructive/40',
+		'flex w-full rounded-md border border-input bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 md:text-sm dark:bg-input/30 dark:aria-invalid:ring-destructive/40',
+		rows === undefined && 'field-sizing-content min-h-16',
 		className
 	)}
 	bind:value
+	{rows}
 	{...restProps}
 ></textarea>
